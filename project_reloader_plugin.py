15f0a7938b6ff778de0fd8f7c3bb2e8ebde9c571
julien.waddle@camptocamp.com <julien.waddle@camptocamp.com>
2020-03-30 15:35:33 +0200

    chore(format) black the code

--- project_reloader_plugin.py
+++ project_reloader_plugin.py
@@ -18,7 +18,9 @@ class ProjectReloaderPlugin(QgsServerFilter):
         super().__init__(server_iface)
         self._reloading = False
         self._last_reload_times = {}
-        QgsMessageLog.logMessage(f"Registering ProjectReloaderPlugin", NAME, level=Qgis.Info)
+        QgsMessageLog.logMessage(
+            f"Registering ProjectReloaderPlugin", NAME, level=Qgis.Info
+        )
         self.serverInterface().registerFilter(self)
 
     def requestReady(self):
@@ -26,26 +28,36 @@ class ProjectReloaderPlugin(QgsServerFilter):
             self.updateProject()
         except Exception as e:
             QgsMessageLog.logMessage(
-                ''.join(traceback.format_exception(*sys.exc_info())),
+                "".join(traceback.format_exception(*sys.exc_info())),
                 NAME,
-                level=Qgis.Critical
+                level=Qgis.Critical,
             )
 
     def updateProject(self):
         if self._reloading:
-            QgsMessageLog.logMessage(f"Project is already reloading, skip.", NAME, level=Qgis.Info)
+            QgsMessageLog.logMessage(
+                f"Project is already reloading, skip.", NAME, level=Qgis.Info
+            )
             return
 
         if not QgsProject.instance().fileName():
-            QgsMessageLog.logMessage(f"Project filename is empty, nothing to do.", NAME, level=Qgis.Info)
+            QgsMessageLog.logMessage(
+                f"Project filename is empty, nothing to do.", NAME, level=Qgis.Info
+            )
             return
         source_project_time = QgsProject.instance().lastModified().toPyDateTime()
         source_project_path = QgsProject.instance().fileName()
         if not source_project_path in self._last_reload_times:
             self._last_reload_times[source_project_path] = source_project_time
         else:
-            QgsMessageLog.logMessage(f"source_project_time: {source_project_time}",  NAME, level=Qgis.Info)
-            QgsMessageLog.logMessage(f"last_reload_time: {self._last_reload_times[source_project_path]}", NAME, level=Qgis.Info)
+            QgsMessageLog.logMessage(
+                f"source_project_time: {source_project_time}", NAME, level=Qgis.Info
+            )
+            QgsMessageLog.logMessage(
+                f"last_reload_time: {self._last_reload_times[source_project_path]}",
+                NAME,
+                level=Qgis.Info,
+            )
 
             if source_project_time > self._last_reload_times[source_project_path]:
                 self._reloading = True
